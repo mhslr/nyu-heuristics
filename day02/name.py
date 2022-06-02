962269eec73342919cc3445ad872d5098b706a68
Matthias Hasler <matthias@hasler.fr>
2022-06-02 13:06:16 +0200

    python2 -> py3

--- day02/name.py
+++ day02/name.py
@@ -2,7 +2,7 @@
 #
 print("hello, NYU") 
 
-x = raw_input ("Dude, what is your name? ")
+x = input ("Dude, what is your name? ")
 print("Hello, " , x , "! How are you, " , x , "?")
 print("Hey could you lend me some money " + x +"?")
 print(("Hey could you lend me some money " + x +"?")[::-1])
