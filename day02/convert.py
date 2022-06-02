ceef276e20c351cfcaa6fec1885d5300028cb75c
Matthias Hasler <matthias@hasler.fr>
2022-06-02 14:02:57 +0200

    some more python2 updates

--- day02/convert.py
+++ day02/convert.py
@@ -2,7 +2,7 @@
 # 
 # This program converts from US $ to Euro $ 
 
-us_money = eval(input ("Aaditya asks Money value in US $ "))
+us_money = float(input ("Aaditya asks Money value in US $ "))
 
 euros = us_money / 1.16
 print("US$", us_money, "= Euro ", euros)
