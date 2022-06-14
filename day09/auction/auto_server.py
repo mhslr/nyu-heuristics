c028f1781c6756ed405bb4815e2d6d3fd9c13b9e
Matthias Hasler <matthias@hasler.fr>
2022-06-14 14:25:06 +0200

    123

--- day09/auction/auto_server.py
+++ day09/auction/auto_server.py
@@ -30,41 +30,49 @@ def get_info_for(player_name, base_info):
     return info
 
 
-num_bidders = len(bots)
-base_info = {
-    "item_types": item_types,
-    "items": random.choices(
-        item_types, k=num_bidders * needed_to_win * len(item_types) + 1
-    ),
-    "history": [],
-    "cur_round": 0,
-    "history": [],
-    "players": {name: init_player_info(name) for name, strat in bots},
-}
+winners = []
+for i in range(100):
+    num_bidders = len(bots)
+    base_info = {
+        "item_types": item_types,
+        "items": random.choices(
+            item_types, k=num_bidders * needed_to_win * len(item_types) + 1
+        ),
+        "history": [],
+        "cur_round": 0,
+        "history": [],
+        "players": {name: init_player_info(name) for name, strat in bots},
+    }
 
-state = {name: None for name, strat in bots}
-auction_winner = None
-for cur_round, item in enumerate(base_info["items"]):
-    base_info["cur_round"] = cur_round
 
-    if auction_winner is not None:
-        break
 
-    bids = []
-    for name, strat in bots:
-        player_info = get_info_for(name, base_info)
-        bid, state[name] = strat(deepcopy(player_info), state[name])
+    state = {name: None for name, strat in bots}
+    auction_winner = None
+    for cur_round, item in enumerate(base_info["items"]):
+        base_info["cur_round"] = cur_round
 
-        # round
-        bid = max(0, min(player_info["self"]["budget"], int(bid)))
-        bids.append((bid, random.random(), name))
+        if auction_winner is not None:
+            break
 
-    bid, _, round_winner = max(bids)
-    print(item, bid, round_winner, sep="  \t")
-    base_info['history'].append({"bid": bid, "item": item, "player": round_winner})
-    base_info["players"][round_winner]["budget"] -= bid
-    base_info["players"][round_winner]["item_count"][item] += 1
-    if base_info["players"][round_winner]["item_count"][item] == needed_to_win:
-        auction_winner = round_winner
+        bids = []
+        for name, strat in bots:
+            player_info = get_info_for(name, base_info)
+            bid, state[name] = strat(deepcopy(player_info), state[name])
 
-print(auction_winner)
+            # round
+            bid = max(0, min(player_info["self"]["budget"], int(bid)))
+            bids.append((bid, random.random(), name))
+
+        bid, _, round_winner = max(bids)
+        print(item, bid, round_winner, sep="  \t")
+        base_info['history'].append({"bid": bid, "item": item, "player": round_winner})
+        base_info["players"][round_winner]["budget"] -= bid
+        base_info["players"][round_winner]["item_count"][item] += 1
+        if base_info["players"][round_winner]["item_count"][item] == needed_to_win:
+            auction_winner = round_winner
+
+    print('winner:', auction_winner)
+    print()
+    winners.append(auction_winner)
+from collections import Counter
+print(Counter(winners))
